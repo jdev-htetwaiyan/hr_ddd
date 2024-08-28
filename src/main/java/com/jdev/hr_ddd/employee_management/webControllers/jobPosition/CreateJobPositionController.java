b02de531b8569694b9ad816d39782bd45ebebb68
jdev-htetwaiyan <123062422+jdev-htetwaiyan@users.noreply.github.com>
2024-08-29 00:54:02 +0630

    finished create dept & position api. need to fix duplicate values.

--- src/main/java/com/jdev/hr_ddd/employee_management/webControllers/jobPosition/CreateJobPositionController.java
+++ src/main/java/com/jdev/hr_ddd/employee_management/webControllers/jobPosition/CreateJobPositionController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
